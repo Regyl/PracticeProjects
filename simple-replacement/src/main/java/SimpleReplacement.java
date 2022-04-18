974252bf357ca7819b317bc7653725e704abe438
Regyl <novikovevgeny1@gmail.com>
2022-04-19 01:22:01 +0300

    add simple-replacement module

---
+++ simple-replacement/src/main/java/SimpleReplacement.java
@@ -0,0 +1,11 @@
+
+public class SimpleReplacement {
+
+    private static final String ORIGINAL = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    private static final String REPLACEMENT = "ёйцукенгшщзхъфывапролджэячсмитьбю";
+
+    public static void main(String[] args) {
+
+    }
+
+}
