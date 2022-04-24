d13c707af0fd196ea2ed60d132d9cd4aaf254e27
Regyl <novikovevgeny1@gmail.com>
2022-04-25 01:23:52 +0300

    add vigenere encryption

--- analytic-cipher/src/main/java/AnalyticCipher.java
+++ analytic-cipher/src/main/java/AnalyticCipher.java
@@ -2,18 +2,18 @@
 
 public class AnalyticCipher {
 
-    public static void main(String[] args) {
-        String text = "Прямые каналы утечки данных, требуют непосредственного доступа к техническим средствам информационной системы и данным.";
-        int[] intChars = text.chars().toArray();
-        int[] resultChars = new int[text.length()];
+    private static final String MESSAGE = "После приема подписанного сообщения получатель должен проверить, соответствует ли подпись сообщению.";
+    private static final int[][] KEY = new int[][] {   {14,8,3}, //Key
+                                                        {8,5,2},
+                                                        {3,2,1}};
 
-        int[][] matr = new int[][] {{1,5,0}, //Key
-                {2,3,1},
-                {1,7,8}};
+    public static void main(String[] args) {
+        int[] intChars = MESSAGE.chars().toArray();
+        int[] resultChars = new int[MESSAGE.length()];
 
         for(int i=0; i< intChars.length; i++) {
             for(int j=0; j<3; j++) {
-                resultChars[i] += matr[j][i%3] * intChars[i];
+                resultChars[i] += KEY[j][i%3] * intChars[i];
             }
         }
 
---
+++ second-custom-encryption/src/main/java/SecondCustomEncryption.java
@@ -0,0 +1,45 @@
+public class SecondCustomEncryption {
+
+    private static final String MESSAGE = "К понятию «конфиденциальная информация» тесно примыкают понятия «государственная тайна» и «коммерческая тайна».";
+    //A - 1040, Я - 1071, а - 1072, я - 1103
+    public static void main(String[] args) {
+        System.out.println("Текст до шифрования: " + MESSAGE);
+        String resultMessage = Encryptor.encryptionVigenere(MESSAGE);
+        System.out.println("Текст после шифра Виженера:  " + resultMessage);
+        System.out.println((int) 'А');
+        System.out.println((int) 'я');
+    }
+
+    private static class Encryptor {
+
+        private static final int ALPHABET_STRANGE = 33;
+
+        private static final int FIRST_SHIFT = 4;
+        private static final int SECOND_SHIFT = 9;
+        private static final int THIRD_SHIFT = 2;
+
+        //Шифр Виженера
+        protected static String encryptionVigenere(String message) {
+            message = encryptionCaesar(message, FIRST_SHIFT);
+            message = encryptionCaesar(message, SECOND_SHIFT);
+            return encryptionCaesar(message, THIRD_SHIFT);
+        }
+
+        //Шифр Цезаря
+        protected static String encryptionCaesar(String message, int shift) {
+            StringBuilder sb = new StringBuilder();
+            for(char item : message.toCharArray()) {
+                if(item > 'А' && item < 'я') {
+                    int result = (int) item + shift;
+                    if(result > 'я') {
+                        result -= ALPHABET_STRANGE*2;
+                    }
+                    sb.append((char) result);
+                } else {
+                    sb.append(item);
+                }
+            }
+            return sb.toString();
+        }
+    }
+}
